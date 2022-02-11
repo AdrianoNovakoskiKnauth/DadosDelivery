432d915ee7314174bfe167eba46eecadb419c923
AdrianoEscritorio <adriano_novakoski@hotmail.com>
2022-02-11 15:36:01 -0300

    Alterado arquivo novaExtExcel e extDadosIfood

--- Ifood/novaExtExcel.js
+++ Ifood/novaExtExcel.js
@@ -34,21 +34,26 @@ data.forEach(Uni => { //passa por cada Unidade
     const nameUnidade = obterUnidade(Uni.Unidade) || "Não encontrou unidade"
     Uni.Pedidos.forEach(ped => { //Passa por cada Pedido
         const numPedido = ped.Nº
-        const status = ped.status
+        let status = ped.status? `${ped.status}` : "Indeterminado"
         let dia = ped.dataHora.slice(0,2)
         let mes = obterMes(ped.dataHora.slice(3,ped.dataHora.length-6))
         let date = `${dia}/${mes}/2022`
         const itensDia = ped.Itens
         itensDia.forEach(i => {
             if (i.length === 5) {
-                let codProduto = obterCodigo(i[0])
+                let descProduto = i[0]
+                //console.log(descProduto)
+                /* if (descProduto.indexOf('Este item foi cancelado') !== -1){
+                    descProduto.replace("Este item foi cancelado", "")
+                } */
+                let codProduto = obterCodigo(descProduto)
                 if (codProduto != "Molhos" && codProduto != "Adicional" && codProduto != false){
                     let columnIndex = 1; //diz para começar na primeira coluna
                     ws.cell(rowIndex, columnIndex++).string(nameUnidade)
-                    ws.cell(rowIndex, columnIndex++).number(parseInt(numPedido))
+                    ws.cell(rowIndex, columnIndex++).number(parseInt(numPedido) || 999999)
                     ws.cell(rowIndex, columnIndex++).string(status)
                     ws.cell(rowIndex, columnIndex++).string(date)
-                    ws.cell(rowIndex, columnIndex++).string(i[0])    // Produto
+                    ws.cell(rowIndex, columnIndex++).string(descProduto)    // Produto
                     ws.cell(rowIndex, columnIndex++).number(parseInt(i[1]))    // Quantidade
                     ws.cell(rowIndex, columnIndex++).string(codProduto)           //Código Teknisa
                     //ws.cell(rowIndex, columnIndex++).number(parseInt(i[2].replace(".", ","))).style(styleReal)    // Valor Unitário
@@ -65,9 +70,9 @@ data.forEach(Uni => { //passa por cada Unidade
             }
         })
     })
-    itensDescartados.forEach( item => {
+    /* itensDescartados.forEach( item => {
         console.log(item)
-    })
+    }) */
 });
 wb.write(`Delivery.xlsx`)
 
