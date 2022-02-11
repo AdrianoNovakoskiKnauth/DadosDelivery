3725cd2fc7d98826bf1b381fe13e6c9c096c83e4
adriano_nokoski@hotmail.com <adriano_novakoski@hotmail.com>
2022-02-11 06:09:37 -0300

    Adicionado condicinal para desconsiderar produtos que tem como código (Adicinal, Molhos ou que não foram encontrados).

--- Ifood/novaExtExcel.js
+++ Ifood/novaExtExcel.js
@@ -30,6 +30,7 @@ headingColumnNames.forEach(heading => { //passa por todos itens do array
 let rowIndex = 2; //começa na linha 2
 // Colocando os dados na planilha.
 data.forEach(Uni => { //passa por cada Unidade
+    let itensDescartados = []
     const nameUnidade = obterUnidade(Uni.Unidade) || "Não encontrou unidade"
     Uni.Pedidos.forEach(ped => { //Passa por cada Pedido
         const numPedido = ped.Nº
@@ -40,23 +41,33 @@ data.forEach(Uni => { //passa por cada Unidade
         const itensDia = ped.Itens
         itensDia.forEach(i => {
             if (i.length === 5) {
-                let columnIndex = 1; //diz para começar na primeira coluna
-                ws.cell(rowIndex, columnIndex++).string(nameUnidade)
-                ws.cell(rowIndex, columnIndex++).number(parseInt(numPedido))
-                ws.cell(rowIndex, columnIndex++).string(status)
-                ws.cell(rowIndex, columnIndex++).string(date)
-                ws.cell(rowIndex, columnIndex++).string(i[0])    // Produto
-                ws.cell(rowIndex, columnIndex++).number(parseInt(i[1]))    // Quantidade
-                ws.cell(rowIndex, columnIndex++).string(obterCodigo(i[0]) || "Não encontrado")           //Código Teknisa
-                //ws.cell(rowIndex, columnIndex++).number(parseInt(i[2].replace(".", ","))).style(styleReal)    // Valor Unitário
-                //ws.cell(rowIndex, columnIndex++).number(parseInt(i[3].replace(".", ","))).style(style)    // Incentivo
-                //ws.cell(rowIndex, columnIndex++).number(parseInt(i[4].replace(".", ","))).style(styleReal)    // Subtotal
-                rowIndex++; //incrementa o contador para ir para a próxima linha
-                //console.log(i[0] + " - "+ i[1] + " - "+i[2] + " - "+i[3] + " - "+i[4])
+                let codProduto = obterCodigo(i[0])
+                if (codProduto != "Molhos" && codProduto != "Adicional" && codProduto != false){
+                    let columnIndex = 1; //diz para começar na primeira coluna
+                    ws.cell(rowIndex, columnIndex++).string(nameUnidade)
+                    ws.cell(rowIndex, columnIndex++).number(parseInt(numPedido))
+                    ws.cell(rowIndex, columnIndex++).string(status)
+                    ws.cell(rowIndex, columnIndex++).string(date)
+                    ws.cell(rowIndex, columnIndex++).string(i[0])    // Produto
+                    ws.cell(rowIndex, columnIndex++).number(parseInt(i[1]))    // Quantidade
+                    ws.cell(rowIndex, columnIndex++).string(codProduto)           //Código Teknisa
+                    //ws.cell(rowIndex, columnIndex++).number(parseInt(i[2].replace(".", ","))).style(styleReal)    // Valor Unitário
+                    //ws.cell(rowIndex, columnIndex++).number(parseInt(i[3].replace(".", ","))).style(style)    // Incentivo
+                    //ws.cell(rowIndex, columnIndex++).number(parseInt(i[4].replace(".", ","))).style(styleReal)    // Subtotal
+                    rowIndex++; //incrementa o contador para ir para a próxima linha
+                    //console.log(i[0] + " - "+ i[1] + " - "+i[2] + " - "+i[3] + " - "+i[4])
+                } else {
+                    let item = []
+                    item.push(i[0]) // Descrição do produto
+                    codProduto ? item.push(codProduto) : item.push("Item não encontrado")
+                    itensDescartados.push(item)
+                }
             }
         })
     })
-
+    itensDescartados.forEach( item => {
+        console.log(item)
+    })
 });
 wb.write(`Delivery.xlsx`)
 
