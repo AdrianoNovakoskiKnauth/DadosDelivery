39ccce0bec219744340a27f383ae0338efbdb1e6
adriano_nokoski@hotmail.com <adriano_novakoski@hotmail.com>
2022-02-18 05:23:42 -0300

    Substituido o arqui codigoProduto.json por baseDeProduto.js. Esse novo arquivo contem o nome do produto com código e categoria. Foi corrigido também o arquivo oobterCodigo.js para essa nova estrutura.

--- myModules/obterCodigo.js
+++ myModules/obterCodigo.js
@@ -1,10 +1,10 @@
-const codigoProduto = require('../parameters/codigoProduto.json')
+const codigoProduto = require('../parameters/baseDeProduto.json')
 
 function obterCodigo (produto) {
     let codReturn = false
     for (i in codigoProduto) {
         if (Object.keys(codigoProduto[i])[0].localeCompare(produto, undefined, { sensitivity: 'base'}) == 0) {
-            return codReturn = Object.values(codigoProduto[i])[0]
+            return codReturn = Object.values(codigoProduto[i])[0].codigo
         }
     }
     return codReturn
